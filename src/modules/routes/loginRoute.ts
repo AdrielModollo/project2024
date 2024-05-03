4840006fd74e55d57e8d1a0f1535e4db10968580
Adriel <adriel.modollo@lit.com.br>
2024-05-03 16:28:10 -0300

    feat: add route login

--- src/modules/routes/loginRoute.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { loginController } from '../controllers/loginController';
-
-const router = Router();
-
-router.get('/login', loginController);
-
-export default router;
